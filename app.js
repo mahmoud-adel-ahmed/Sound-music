b1605ed45e642b45bc044ad728b33e2c5da24cbe
mahmoudalghareeb253@gmail.com <tato.alghareeb@gmail.com>
2022-01-06 16:06:59 +0200

    style's still modified

--- app.js
+++ app.js
@@ -8,23 +8,23 @@ window.addEventListener("load", () => {
     "rgb(43, 233, 26)",
     "rgb(231, 63, 33)",
     "rgb(10, 166, 228)",
-    "rgb(199, 233, 7),",
+    "rgb(199, 233, 7)",
   ];
 
   // create sounds
-  pads.forEach((pad, i) => {
+  pads.forEach((pad, index) => {
     pad.addEventListener("click", function () {
-      sounds[i].currentTime = 0;
-      sounds[i].play();
-      makebubbles(i);
+      sounds[index].currentTime = 0;
+      sounds[index].play();
+      makebubbles(index);
     });
   });
 
   // create bubbles
-  const makebubbles = (i) => {
+  const makebubbles = (index) => {
     const bubble = document.createElement("div");
     visual.appendChild(bubble);
-    bubble.style.backgroundColor = colors[i];
+    bubble.style.backgroundColor = colors[index];
     bubble.style.animation = "jump 1s linear";
     bubble.addEventListener("animationend", function () {
       visual.removeChild(this);
